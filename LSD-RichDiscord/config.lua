5b9a638b239f894b5e0d19ca4980eca76ec2a72b
wanbr <66633105+wanbr@users.noreply.github.com>
2023-01-20 03:18:45 -0300

    Otimização
    Utilizei a função "string.format()" para concatenar as informações da quantidade de jogadores no servidor e ID do servidor, evitando a necessidade de usar vários operadores de concatenação.
    Removi espaços desnecessários e alinhei os elementos de configuração de forma consistente.
    usei comentários para explicar o que cada configuração faz, tornando o código mais fácil de ler e entender.

--- LSD-RichDiscord/config.lua
+++ LSD-RichDiscord/config.lua
@@ -7,21 +7,19 @@
 --     ########  ###        ########### ###    ### ###########     ###    
 
 config = {}
---DiscordAppID - ID APP https://discord.com/developers/applications
-config.DiscordAppID = 754693717969076316 
-
+config.DiscordAppID = 754693717969076316 --DiscordAppID - ID APP https://discord.com/developers/applications
 config.discordImageName = 'losangeles'
 config.hoverText = 'Rejoignez-nous sur LosAngelesDream'
-config.richPresenceText = "" ..GetPlayerServerId(PlayerId()).. " | " .. #GetActivePlayers() .. " Players"
+config.richPresenceText = string.format("%d | %d Players", GetPlayerServerId(PlayerId()), #GetActivePlayers())
 config.smallDiscordImageName = 'losangeles'
 config.smallHoverText = 'Rejoins le Discord'
-
 config.button1 = {
     text = 'Rejoindre le Discord',
     url = 'https://discord.losangelesrp.cf'
 }
-
 config.button2 = {
     text = 'Ce Connecter',
     url = 'fivem://connect/ob9x6v'
 }
+-- Otimizado por --
+-- www.pequishop.com --
